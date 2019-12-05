8be6499f64eeba599603a7089b97baa464a71b3a
John <john@real-currents.com>
2019-12-05 07:09:27 -0500

    update cube env

--- main.ts
+++ main.ts
@@ -40,7 +40,7 @@ function main () {
 
     backgroundUpdater(gl);
 
-    // Get A 2D context
+    // Get A 2D context for dynamic textures
     /** @type {Canvas2DRenderingContext} */
     const ctx = document.createElement("canvas").getContext("2d");
     ctx.canvas.width = 128;
@@ -50,46 +50,29 @@ function main () {
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
 
-    const video = (window.document.querySelector('#aud1') !== null) ?
-        window.document.querySelector('#aud1') as HTMLVideoElement :
-        document.createElement('video');
-
-    if (video.id !== 'aud1') {
-        const source1 = document.createElement('source');
-        const source2 = document.createElement('source');
-        source1.src = "data/fathers.mp4";
-        source2.src = "data/fathers.ogv";
-        video.append(source1);
-        video.append(source2);
-
-    } else {
-        /*
-<video id="aud1" preload="auto" muted ="true" controls="true">
-  <source src="https://s3-us-west-1.amazonaws.com/real-currents/js-demos/video/fathers.mp4" />
-  <source src="https://s3-us-west-1.amazonaws.com/real-currents/js-demos/video/fathers.ogv" />
-</video>
-         */
-        window['aud1'] = video;
-    }
-
-    let videoLoad = false;
-    let videoReady = false;
-    let videoName = (<HTMLSourceElement>video.children[0]).src.match(/[\/|\\]*([\w|\-|]+)\.\w\w\w$/)[1];
-    video.pause();
-
-    console.log(videoName);
-
     let frame = 0;
     let timeout = null;
     let lastKeyPress = (new Date()).getTime();
-    let startVideo = false;
-    let stopVideo = false;
     window['userTriggered'] = false;
 
     const aCanvas = document.createElement('canvas');
     const bCanvas = document.createElement('canvas');
-    aCanvas.width = bCanvas.width = ctx.canvas.width;
-    aCanvas.height = bCanvas.height = ctx.canvas.height;
+    const cCanvas = document.createElement('canvas');
+    const dCanvas = document.createElement('canvas');
+    const eCanvas = document.createElement('canvas');
+    const fCanvas = document.createElement('canvas');
+    aCanvas.width =
+        bCanvas.width =
+            cCanvas.width =
+                dCanvas.width =
+                    eCanvas.width =
+                        fCanvas.width = ctx.canvas.width;
+    aCanvas.height =
+        bCanvas.height =
+            cCanvas.height =
+                dCanvas.height =
+                    eCanvas.height =
+                        fCanvas.height = ctx.canvas.height;
 
     const faceInfos = [
         { target: gl.TEXTURE_CUBE_MAP_POSITIVE_X, faceColor: '#F00', textColor: '#0FF', text: '+X' },
@@ -103,13 +86,21 @@ function main () {
 
     faceInfos.forEach((faceInfo, i, a) => {
         const { target, faceColor, textColor, text } = faceInfo;
+        // Asynchronously load an image
         const img = new Image();
         img.crossOrigin = '';
 
         img.id = '' + (i + 1);
 
-        // Use 2d face generator to generate 6 images
-        generateFace(ctx, faceColor, textColor, text);
+        // Initialize offscreen canvases with face generator to generate 6 images
+        const offscreen_ctx = (+img.id === 1) ? aCanvas.getContext('2d') :
+            (+img.id === 2) ? bCanvas.getContext('2d') :
+                (+img.id === 3) ? cCanvas.getContext('2d') :
+                    (+img.id === 4) ? dCanvas.getContext('2d') :
+                        (+img.id === 5) ? eCanvas.getContext('2d') :
+                            fCanvas.getContext('2d');
+        offscreen_ctx.globalAlpha = 1.0;
+        generateFace(offscreen_ctx, faceColor, textColor, text);
 
         // Upload the canvas to the cubemap face.
         const level = 0;
@@ -119,12 +110,6 @@ function main () {
         const width = 512;
         const height = 512;
 
-        // Setup each face so it's immediately renderable
-        gl.texImage2D(target, level, internalFormat, width, height, 0, format, type, null);
-
-        // Asynchronously load an image
-        // const img = new Image();
-        img.src = video.poster;
         img.style.margin = 'auto';
         img.style.position = 'fixed';
         img.style.top = '0px';
@@ -137,98 +122,40 @@ function main () {
         });
         // document.body.appendChild(img);
 
-        // show the result
-        ctx.canvas.toBlob((blob) => {
-            console.log(`Loading ${img.id} ...`);
-            img.src = URL.createObjectURL(blob);
-            img.style.margin = 'auto';
-            img.style.position = 'fixed';
-            img.style.top = '0px';
-            img.style.left = i * ctx.canvas.width + 'px';
-        });
-
         setInterval(d => {
-            if (frame > 0) {
-                if ((+img.id) % 2) ctx.drawImage(aCanvas, 0, 0, aCanvas.width, aCanvas.height);
-                else ctx.drawImage(bCanvas, 0, 0, bCanvas.width, bCanvas.height);
-
-                // show the result
-                if (video.currentTime > 0) ctx.canvas.toBlob((blob) => {
-                    img.src = URL.createObjectURL(blob);
-                });
-
-            } else {
-                img.src = video.poster;
+            // Update texture from off-screen canvases
+            switch ((+img.id)) {
+                case 1:
+                    ctx.drawImage(aCanvas, 0, 0, aCanvas.width, aCanvas.height);
+                    break;
+                case 2:
+                    ctx.drawImage(bCanvas, 0, 0, bCanvas.width, bCanvas.height);
+                    break;
+                case 3:
+                    ctx.drawImage(cCanvas, 0, 0, cCanvas.width, cCanvas.height);
+                    break;
+                case 4:
+                    ctx.drawImage(dCanvas, 0, 0, dCanvas.width, dCanvas.height);
+                    break;
+                case 5:
+                    ctx.drawImage(eCanvas, 0, 0, eCanvas.width, eCanvas.height);
+                    break;
+                default:
+                    ctx.drawImage(fCanvas, 0, 0, fCanvas.width, fCanvas.height);
             }
+
+            // show the result
+            ctx.canvas.toBlob((blob) => {
+                img.src = URL.createObjectURL(blob);
+            });
+
         }, 66);
+
+        // Setup each face so it's immediately renderable
+        gl.texImage2D(target, level, internalFormat, width, height, 0, format, type, null);
     });
     gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
     gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-
-    const videoPlayer = function () {
-        if (!stopVideo && (startVideo || video.paused || !window['userTriggered'])) {
-            startVideo = true;
-
-            lastKeyPress = (new Date()).getTime();
-
-            console.log("Play video");
-
-            video.play();
-
-            /*! Avoid any references to ctx (final buffer) within this routine !*/
-
-            const actx = aCanvas.getContext('2d');
-            actx.globalAlpha = 1.0;
-            const bctx = bCanvas.getContext('2d');
-            bctx.globalAlpha = 1.0;
-
-            const vh = aCanvas.height;
-            const vw = video.videoHeight * (aCanvas.width / aCanvas.height) / 2;
-            const vax = 0;
-            const vbx = -(vw - aCanvas.width) / 1.75;
-
-            timeout = setInterval(d => { // requestAnimationFrame(d => {
-                if ((video != null) && (video.readyState > 2) && (!video.paused)) {
-                    if (++frame % 2) {
-                        actx.drawImage(video, vax, 0, vw, vh);
-                    } else {
-                        bctx.drawImage(video, vbx, 0, vw, vh);
-                    }
-                }
-
-                if (video.currentTime > 5 && !video.muted) {
-                    updateContext(gl, {
-                        'buffers': innerBuffers,
-                        'cameraDelta': [ 0, 0, +0.05 ],
-                        'viewPosition': null,
-                        'worldCameraPosition': [ 0, 0, -1 ]
-                    });
-                } else if (video.currentTime > 1 && !video.muted) {
-                    updateContext(gl, {
-                        'cameraDelta': [ 0, 0, +0.025 ],
-                        'viewPosition': [ 0, 0, -1 ],
-                        'worldCameraPosition': [ 0, 0, -1 ]
-                    });
-                }
-            }, 33);
-
-            setTimeout(s => startVideo = false, 1000);
-
-        } else if (!startVideo) {
-            console.log("Pause video!");
-            video.pause();
-
-            stopVideo = true;
-
-            if (timeout != null) {
-                // cancelAnimationFrame(timeout);
-                clearTimeout(timeout);
-                timeout = null;
-            }
-
-            setTimeout(s => stopVideo = false, 1000);
-        }
-    };
 
     window.addEventListener('keydown', function (event: (any | KeyboardEvent)) {
         const kbEvent = (event || window['event']); // cross-browser shenanigans
@@ -236,17 +163,29 @@ function main () {
         // console.log(startVideo);
 
         if (kbEvent['keyCode'] === 32 && ((new Date()).getTime() - lastKeyPress) > 500) { // this is the spacebar
-            videoPlayer();
-
             if (!window['userTriggered']) {
-                video.currentTime = 0;
-                video.muted = false;
-                window['userTriggered'] = true;
-            }
+                timeout = setInterval(() => {
+                        window['userTriggered'] = true;
 
-            setTimeout( d => {
-                if (kbEvent == KeyboardEvent) video.muted = false;
-            }, 33);
+                        if (++frame > 100) {
+                            updateContext(gl, {
+                                'buffers': innerBuffers,
+                                'cameraDelta': [0, 0, +0.05],
+                                'viewPosition': null,
+                                'worldCameraPosition': [0, 0, -1]
+                            });
+                        } else {
+                            updateContext(gl, {
+                                'cameraDelta': [0, 0, +0.035],
+                                'viewPosition': [0, 0, -1],
+                                'worldCameraPosition': [0, 0, -1]
+                            });
+                        }
+                    }, 33);
+            } else {
+                window['userTriggered'] = false;
+                clearInterval(timeout);
+            }
 
             kbEvent.preventDefault();
 
@@ -254,16 +193,6 @@ function main () {
 
         } else return false;
     });
-
-    window['playVideo'] = videoPlayer;
-
-    // setTimeout(() => {
-    //     if (!!video.paused) {
-    //         video.muted = true;
-    //         videoPlayer();
-    //     }
-    // }, 500);
-
 }
 
 main();
